d12752f100196a8a254461bd8f8dd66906749c88
sergeyTurundayev <sergey.turundayev@gmail.com>
2021-02-22 19:20:25 +0200

    commit

--- parser.php
+++ parser.php
@@ -5,6 +5,8 @@ ini_set('error_reporting', E_ALL);
 
 require 'vendor/autoload.php';
 
+require 'surah_ayah_count.php';
+
 use DiDom\Document;
 
 $document = new Document('https://read.tafsir.one/almukhtasar#pg_-1', true);
@@ -18,9 +20,8 @@ foreach($divs as $div) {
     $saArrkey = explode(',', $div->getAttribute('data-sa') )[0];
     $saArrValue = explode(',', $div->getAttribute('data-sa') )[1];
 
-    $saArr[ $saArrkey ][] = $saArrValue;
+    $saArr[] = [$saArrkey, $saArrValue];
 }
-
 
 $file = 'result.json';
 
@@ -30,29 +31,33 @@ $mainUrl = 'https://read.tafsir.one/get.php?uth&src=almukhtasar';
 
 $saObj = [];
 
-foreach ($saArr as $key => $value) {
+for ($i=0; $i < count( $saArr ); $i++) {
 
-    foreach ($value as $valueKey) {
+    $surah = $saArr[$i][0];
 
-        echo $key . ':' . $valueKey . ', ';
+    $fromAyah =  1*$saArr[$i][1];
 
-        $pageUrl = $mainUrl . '&s=' . $key . '&a=' . $valueKey;
+    $pageUrl = $mainUrl . '&s=' . $surah . '&a=' . $fromAyah;
 
-        $page = file_get_contents($pageUrl);
+    $page = file_get_contents($pageUrl);
 
-        $pageJson = json_decode($page);
+    $pageJson = json_decode($page);
 
-        $pageJsonData = $pageJson->{'data'};
+    $pageJsonData = $pageJson->data;
 
-        $pageJsonData =  str_replace('\n', PHP_EOL, $pageJsonData);
+    $pageJsonData =  str_replace('\n', PHP_EOL, $pageJsonData);
 
-        $saObj[$key][$valueKey] = $pageJsonData;
+    if( $surah == 114 || $saArr[$i+1][1] == 1){
+        $toAyah = surah_ayah_count( $saArr[$i][0]);
+    } else {
+        $toAyah = $saArr[$i+1][1] - 1;
     }
 
+    // echo $surah . ': ' . $fromAyah . '-' . $toAyah . PHP_EOL;
+
+    $saObj[$surah][] = ['from:' => $fromAyah, 'to:' => $toAyah, 'text:' => $pageJsonData];
 }
 
 $result = json_encode($saObj, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
 file_put_contents($file, $result);
-
-die();
